848d66fda67a87e29a65932a13798a79b8f14c77
Ji Kim <jkim55@gmail.com>
2016-04-09 15:11:52 -0600

    enigma test edit

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,25 +6,25 @@ class EnigmaTest < Minitest::Test
   def test_it_can_encrypt_a_message
     message = "hello"
     eg = Enigma.new
-    assert_equal "*IDX<", eg.encrypt(message, 12345, "030416")
+    assert_equal "*IDX<", eg.encrypt(message, "12345", "030416")
   end
 
   def test_it_can_decrypt_a_message
     message = "*IDX<"
     dg = Enigma.new
-    assert_equal "hello", dg.decrypt(message, 12345, "030416")
+    assert_equal "hello", dg.decrypt(message, "12345", "030416")
   end
 
   def test_it_can_encrypt_a_message
     message = "Olive is a cat who lives in DTC"
     eg2 = Enigma.new
-    assert_equal "LRDIbqDF7Gf2XZfJeUf.fb,F7OIXA,k", eg2.encrypt(message, 54321, "100615")
+    assert_equal "LRDIbqDF7Gf2XZfJeUf.fb,F7OIXA,k", eg2.encrypt(message, "54321", "100615")
   end
 
   def test_it_can_decrypt_a_message
     message = "LRDIbqDF7Gf2XZfJeUf.fb,F7OIXA,k"
     dg = Enigma.new
-    assert_equal "Olive is a cat who lives in DTC", dg.decrypt(message, 54321, "100615")
+    assert_equal "Olive is a cat who lives in DTC", dg.decrypt(message, "54321", "100615")
   end
 
 
